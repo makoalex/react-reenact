7fc0fc4cae9f877d942cb9020a81b707d0df7f0d
makoalex <macovei.a2@gmail.com>
2025-05-05 13:46:16 +0200

    Refactoring Player component

--- src/assets/components/Player.jsx
+++ src/assets/components/Player.jsx
@@ -3,24 +3,26 @@ import Button from "./Button";
 import { useState } from "react";
 
 export default function Player({ name, info, ...props }) {
-    const [isEditing, setIsEditing] = useState(false);
-const handleEditClick = () => {
-  
+  const [isEditing, setIsEditing] = useState(false);
+  const handleEditClick = () => {
     setIsEditing(true);
-}
+  };
+  let playerName = <span className="player-name">{name}</span>;
+  
+  if (isEditing) {
+    playerName = (
+      <input className="name-field" type="text" placeholder="Enter new name" />
+    );
+  }
 
   return (
     <li>
       <span {...props}>
-        {isEditing? <input className="name-field" type="text" placeholder="Enter new name" /> 
-        :
-        <>
-        <span className="player-name">{name}</span>
-      </>}
+        {playerName}
         <span className="player-symbol">{info}</span>
       </span>
-      
-      <Button onSelect = {handleEditClick} className="edit-button">
+
+      <Button onSelect={handleEditClick} className="edit-button">
         Edit
       </Button>
     </li>
